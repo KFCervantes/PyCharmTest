a623fb91df90121f1a33857b256ebe27a0e2b665
kcervantes <kfcervantes@gmail.com>
2024-11-12 09:09:29 -0800

    Added Main

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import cmath
+
+def myPow(x, y):
+    return cmath.exp(y * cmath.log(x))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(myPow(-1, 0.5))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
